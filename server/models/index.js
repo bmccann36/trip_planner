1eebe5051cbe23e25ae25625d8869f3600567da1
Oleksii Borysonok <alex.borysonok@gmail.com>
2017-10-05 10:04:06 -0400

    step_seeding

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const db = require('./_db');
-
+const Itinerary = require('./itinerary');
 const Place = require('./places');
 const Restaurant = require('./restaurants')
 const Activity = require('./activities');
@@ -9,6 +9,10 @@ const Hotel = require('./hotels');
 Hotel.belongsTo(Place);
 Restaurant.belongsTo(Place);
 Activity.belongsTo(Place);
+
+Itinerary.belongsToMany(Hotel, {through: 'itinerary_hotel'});
+Itinerary.belongsToMany(Restaurant, {through: 'itinerary_restaurant'});
+Itinerary.belongsToMany(Activity, {through: 'itinerary_activity'});
 
 module.exports = {
 	db,
---
+++ server/models/itinerary.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize');
+const db = require('./_db');
+
+const Itinerary = db.define('itinerary',{
+
+});
+
+// Hotel.belongsTo(Place);
+
+
+module.exports = Itinerary;
